cc362a3f4503544fe3262ff58807d1f0e86685ba
Edgar Hurtado <edhurtado@utp.edu.co>
2025-02-24 10:26:06 -0500

    - Adding skeleton for functions in app.js file

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Defining base array to store all friends' names
+let friends = [];
+
+// Defining base functions for challenge: initially we'd need 3: addFriends, updateFriends and sortFriends/assignFriends
+function addFriends(){
+    // Code logic to be defined
+
+}
+
+function updateFriends(){
+    // Code logic to be defined
+
+}
+
+function sortFriends(){
+    // Code logic to be defined
+
+}
